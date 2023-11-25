ca97388d3f12c8c0b61dd4ab0dbeadbb904c4bfa
TUCJVXCB <ttanyuu@hotmail.com>
2023-11-25 21:11:13 +0800

    init

---
+++ src/main/kotlin/com/tanyu/krule/KRuleApplication.kt
@@ -0,0 +1,11 @@
+package com.tanyu.krule
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KRuleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KRuleApplication>(*args)
+}
---
+++ src/test/kotlin/com/tanyu/krule/krule/KRuleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tanyu.krule.krule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KRuleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
